18268b0b3284e9da32e55c100787e6c8785c6a8f
Ester <kocabova@mamiart.cz>
2026-01-02 22:42:38 +0100

    update 2.1. styles, images, content

--- script/den copy.js
+++ script/den copy.js
@@ -1,12 +1,10 @@
 
-    var myText = '17. 6. 2023 ve 13:30 Kostel sv. Matouše Deštné v Orl. h.';
+    var myText = 'Obřad bude probíhat dopoledne v kostele sv. Jana a Pavla ve Frýdku-Místku. Poté bude následovat přejezd na svatební hostinu na Chatě Dukle na Čeladné (20km). Přesný harmonogram bude brzy upřesněn na našem webu.';
     var myArray = myText.split("");
     var loopTimer;
     var targetElement = document.getElementById("type_obrad");
-  
     // Clear existing text in the target element
     targetElement.innerHTML = '';
-  
     function frameLooper() {
       if(myArray.length > 0) {
         targetElement.innerHTML += myArray.shift();
@@ -16,8 +14,4 @@
       }
       loopTimer = setTimeout(frameLooper, 70);
     }
-  
     frameLooper();
-  
-
-  
